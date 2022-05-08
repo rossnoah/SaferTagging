dbe0b6b4512038bedf7a95608ef26a125c8e6d0f
PhoenixZTT <phoenix@vanillaplus.net>
2022-05-08 00:34:02 -0700

    Optimized plugin

--- src/main/java/net/vanillaplus/safertagging/Main.java
+++ src/main/java/net/vanillaplus/safertagging/Main.java
@@ -4,51 +4,31 @@ import com.SirBlobman.combatlogx.api.event.PlayerPreTagEvent;
 import com.SirBlobman.combatlogx.api.event.PlayerReTagEvent;
 import com.booksaw.betterTeams.Team;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin implements Listener {
+import java.util.UUID;
 
+public final class Main extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
-        // Plugin startup logic
-        Bukkit.getPluginManager().registerEvents(this,this);
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
+
     @EventHandler
     public void beforeTag(PlayerPreTagEvent e) {
         Player tagged = e.getPlayer();
         if (e.getEnemy() instanceof Player) {
-            Player enemy = (Player) e.getEnemy();
-            if (Team.getTeam(tagged) != null&&Team.getTeam(enemy)!=null) {
-                if(Team.getTeam(tagged).equals(Team.getTeam(enemy))){
-
-            e.setCancelled(true);
+            Player enemy = (Player)e.getEnemy();
+            if (Team.getTeam(tagged.getUniqueId()) != null && Team.getTeam(enemy.getUniqueId()) != null &&
+                    Team.getTeam(tagged.getUniqueId()).equals(Team.getTeam(enemy.getUniqueId())))
+                e.setCancelled(true);
         }
     }
-    }
-    }
-
-    @EventHandler
-    public void reTag(PlayerReTagEvent e){
-
-        e.setCancelled(true);
-
-
-
-        Player tagged = e.getPlayer();
-        if (e.getEnemy() instanceof Player) {
-            Player enemy = (Player) e.getEnemy();
-            if (Team.getTeam(tagged) != null&&Team.getTeam(enemy)!=null) {
-                if(Team.getTeam(tagged).equals(Team.getTeam(enemy))){
-
-                    e.set(true);
-                }
-            }
-        }
-    }
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+    @Override
+    public void onDisable() {}
 }
